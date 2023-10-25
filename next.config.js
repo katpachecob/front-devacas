e8800e7e1c3d776b700cabd1380e9dd09d3e4240
KatherinePacheco <89138370+katpachecob@users.noreply.github.com>
2023-10-25 12:00:34 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'http://ec2-3-23-130-140.us-east-2.compute.amazonaws.com/:path*',
-          },
-        ]
-      },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
